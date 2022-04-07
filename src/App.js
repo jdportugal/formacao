867cb663207da2e635ab37c1612fc2527ad3368f
jd-portugal <joaodiogocferreira@gmail.com>
2022-04-07 19:43:14 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useStopwatch } from "react-timer-hook";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyStopwatch() {
+	const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
+		useStopwatch({ autoStart: true });
+
+	const valuePerSecond = 20 / 60 / 60;
+
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>Tempo (de formação) é dinheiro</h1>
+			<div style={{ fontSize: "100px" }}>
+				<span>{hours}h</span>:<span>{minutes}m</span>:<span>{seconds}s</span>
+			</div>
+			<div>
+				<span>
+					{(
+						valuePerSecond * seconds +
+						valuePerSecond * 60 * minutes +
+						valuePerSecond * 60 * 60 * hours
+					).toFixed(4) + " "}
+					€
+				</span>
+			</div>
+			<p>{isRunning ? "Running" : "Not running"}</p>
+			<button onClick={start}>Start</button>
+			<button onClick={pause}>Pause</button>
+			<button onClick={reset}>Reset</button>
+		</div>
+	);
 }
 
-export default App;
+export default function App() {
+	return (
+		<div>
+			<MyStopwatch />
+		</div>
+	);
+}
